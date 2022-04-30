80ad626ed04a5b30f719e1abcf6451a2490a4e06
anupamapan9 <anupamapan0905@gmail.com>
2022-04-30 13:21:53 +0600

    delete item added

--- index.js
+++ index.js
@@ -50,6 +50,15 @@ async function run() {
             const result = await bookCollection.insertOne(book)
             res.send(result)
         })
+
+
+        // delete a data by id 
+        app.delete('/books/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await bookCollection.deleteOne(query)
+            res.send(result)
+        })
     } finally {
 
     }
